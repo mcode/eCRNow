fc81fab07fe721e51150e6a400340507265643f8
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-08-16 16:38:42 -0400

    swap order of operations

--- src/main/java/com/drajer/bsa/service/impl/KarParserImpl.java
+++ src/main/java/com/drajer/bsa/service/impl/KarParserImpl.java
@@ -1,6 +1,7 @@
 package com.drajer.bsa.service.impl;
 
 import ca.uhn.fhir.parser.IParser;
+import com.drajer.bsa.ehr.subscriptions.SubscriptionGenerator;
 import com.drajer.bsa.kar.action.EvaluateMeasure;
 import com.drajer.bsa.kar.action.SubmitReport;
 import com.drajer.bsa.kar.action.ValidateReport;
@@ -95,6 +96,8 @@ public class KarParserImpl implements KarParser {
 
   @Value("${fhirpath.enabled:true}")
   Boolean fhirpathEnabled;
+
+  @Autowired SubscriptionGenerator subscriptionGenerator;
 
   @Autowired BsaServiceUtils utils;
 
@@ -252,6 +255,7 @@ public class KarParserImpl implements KarParser {
       PlanDefinition plan, KnowledgeArtifact art, File karBundleFile) {
 
     processExtensions(plan, art);
+    subscriptionGenerator.subscriptionsFromPlanDef(plan);
     List<PlanDefinitionActionComponent> actions = plan.getAction();
 
     for (PlanDefinitionActionComponent act : actions) {
